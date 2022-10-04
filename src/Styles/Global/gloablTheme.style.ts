1dd3292c124d99a99d7770d95067281a56a1e2ee
jisoung <jangjisoung0405@gmail.com>
2022-10-04 22:07:06 +0900

    init

---
+++ src/Styles/Global/gloablTheme.style.ts
@@ -0,0 +1,14 @@
+import { DefaultTheme } from "styled-components";
+
+export const baseTheme: DefaultTheme = {
+  Primary: "#80CCE3",
+  Primary2: "#5596AA",
+  Primary3: "#C7EDF9",
+  Primary4: "#E2F8FF",
+  HighlightRed: "#FF3232",
+  HighlightRed700: "#FF6060",
+  Hint1: "#888888",
+  Black: "#000000",
+  white: "#FFFFFF",
+  white200: "#DDDDDD",
+};
---
+++ src/Styles/Global/globalThemeType.style.d.ts
@@ -0,0 +1,15 @@
+import "styled-components";
+declare module "styled-components" {
+  export interface DefaultTheme {
+    Primary: string;
+    Primary2: string;
+    Primary3: string;
+    Primary4: string;
+    HighlightRed: string;
+    HighlightRed700: string;
+    Black: string;
+    white: string;
+    white200: string;
+    Hint1: string;
+  }
+}
