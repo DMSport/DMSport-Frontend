afce217e67c41b1a22a88f3db78658315bb92a42
JJIIIINN <kgb21537@gmail.com>
2022-10-25 18:55:29 +0900

    docs :: index 수정

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import Footer from "./Components/Footer";
 import { RecoilRoot } from "recoil";
 import { ThemeProvider } from "styled-components";
 import { baseTheme } from "./Styles/Global/gloablTheme.style";
