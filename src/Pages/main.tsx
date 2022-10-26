408cef9b8a023ba805ea72abfbf4996b13251b42
jisoung <jangjisoung0405@gmail.com>
2022-10-26 23:12:15 +0900

    Feat : 컴포 추가

--- src/Pages/main.tsx
+++ src/Pages/main.tsx
@@ -1,5 +1,15 @@
+import First from "../Components/Main/First";
+import Second from "../Components/Main/Second";
+import Third from "../Components/Main/Third";
+
 function Main() {
-  return <div>main</div>;
+  return (
+    <>
+      <First />
+      <Second />
+      <Third />
+    </>
+  );
 }
 
 export default Main;
