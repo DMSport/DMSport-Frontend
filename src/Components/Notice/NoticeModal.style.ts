85081f67c73cd709fe82f27300c8c984d1075ed5
jikwan <jikwan0327@gmail.com>
2022-10-30 21:55:17 +0900

    docs :: 스타일 수정

--- src/Components/Notice/NoticeModal.style.ts
+++ src/Components/Notice/NoticeModal.style.ts
@@ -1,1 +1,67 @@
 import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+//모달 앞부분 중앙
+export const White = styled.div`
+  position: fixed;
+  width: 930px;
+  height: 450px;
+  background-color: ${(props) => props.theme.white};
+  z-index: 101;
+  overflow-y: scroll;
+  white-space: pre-wrap;
+`;
+
+//모달 뒷부분 검은색
+export const Background = styled.div`
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  background-color: rgba(0, 0, 0, 0.6);
+  z-index: 100;
+`;
+
+//제목과 남은 시간을 같은 줄에 위치하도록 사용
+export const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  height: 70px;
+  margin-top: 20px;
+  margin-left: 70px;
+  border-bottom: 1px solid;
+`;
+
+//제목
+export const Title = styled.div`
+  font-size: 24px;
+  font-weight: 700;
+`;
+
+//시간
+export const Time = styled.div`
+  position: absolute;
+  right: 30px;
+  font-size: 20px;
+  font-weight: 400;
+`;
+
+export const Notice = styled.div`
+  width: 700px;
+  margin-left: 70px;
+  margin-top: 30px;
+  font-size: 24px;
+  line-height: 29px;
+  font-weight: 500;
+`;
--- src/Components/Notice/NoticeModal.tsx
+++ src/Components/Notice/NoticeModal.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { useSetRecoilState } from "recoil";
-import styled from "styled-components";
 import { isNoticeModalAtom } from "../../Utils/atoms";
+import * as _ from "./NoticeModal.style";
 
 function NoticeModal() {
   const setNoticeModalAtom = useSetRecoilState(isNoticeModalAtom);
@@ -18,83 +18,17 @@ function NoticeModal() {
   }, []);
 
   return (
-    <Container>
-      <Background onClick={toggleNoticeModalAtom} />
-      <White>
-        <Wrapper>
-          <Title>제목</Title>
-          <Time>1시간전</Time>
-        </Wrapper>
-        <Notice>오늘은 동아리가 없어요</Notice>
-      </White>
-    </Container>
+    <_.Container>
+      <_.Background onClick={toggleNoticeModalAtom} />
+      <_.White>
+        <_.Wrapper>
+          <_.Title>제목</_.Title>
+          <_.Time>1시간전</_.Time>
+        </_.Wrapper>
+        <_.Notice>오늘은 동아리가 없어요</_.Notice>
+      </_.White>
+    </_.Container>
   );
 }
 
 export default NoticeModal;
-
-const Container = styled.div`
-  width: 100vw;
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-//모달 앞부분 중앙
-const White = styled.div`
-  position: fixed;
-  width: 930px;
-  height: 450px;
-  background-color: ${(props) => props.theme.white};
-  z-index: 101;
-  overflow-y: scroll;
-  white-space: pre-wrap;
-`;
-
-//모달 뒷부분 검은색
-const Background = styled.div`
-  position: fixed;
-  width: 100%;
-  height: 100%;
-  overflow: hidden;
-  top: 0;
-  bottom: 0;
-  right: 0;
-  left: 0;
-  background-color: rgba(0, 0, 0, 0.6);
-  z-index: 100;
-`;
-
-//제목과 남은 시간을 같은 줄에 위치하도록 사용
-const Wrapper = styled.div`
-  display: flex;
-  align-items: center;
-  height: 70px;
-  margin-top: 20px;
-  margin-left: 70px;
-  border-bottom: 1px solid;
-`;
-
-//제목
-const Title = styled.div`
-  font-size: 24px;
-  font-weight: 700;
-`;
-
-//시간
-const Time = styled.div`
-  position: absolute;
-  right: 30px;
-  font-size: 20px;
-  font-weight: 400;
-`;
-
-const Notice = styled.div`
-  width: 700px;
-  margin-left: 70px;
-  margin-top: 30px;
-  font-size: 24px;
-  line-height: 29px;
-  font-weight: 500;
-`;
