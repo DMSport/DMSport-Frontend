7a5713e2ac4471c4bd622e1bd85a81ef8f7b400b
jisoung <jangjisoung0405@gmail.com>
2022-11-09 20:27:03 +0900

    Feat : soccerPage 추가 Feat : 유저의 선택에 따라 border 출력

--- src/Components/Club/ClubPage.tsx
+++ src/Components/Club/ClubPage.tsx
@@ -1,19 +1,29 @@
+import { Link, useLocation } from "react-router-dom";
 import MoonIcon from "../../Assets/SVG/moonIcon";
 import * as _ from "./style";
 
 export default function ClubPage({ clubName }: { clubName: string }) {
+  const { pathname } = useLocation();
+  console.log(pathname);
   return (
     <_.Container>
-      <SideBar />
+      <SideBar pathname={pathname} />
       {clubName === "badminton" && <BadmintonPage />}
+      {clubName === "soccer" && <SoccerPage />}
     </_.Container>
   );
 }
-
+function SoccerPage() {
+  return (
+    <_.SoccerContainer>
+      <img src={require("../../Assets/PNG/soccerBg.png")} alt="" height={600} />
+    </_.SoccerContainer>
+  );
+}
 function BadmintonPage() {
   return (
     <>
-      <_.MainContainer>
+      <_.BadmintonContainer>
         <img
           src={require("../../Assets/PNG/badmintonBg.png")}
           height="600"
@@ -22,87 +32,101 @@ function BadmintonPage() {
           }}
           alt=""
         />
-        <_.MainSpotContainer>
+        <_.BadmintonSpotContainer>
           {[1, 2, 3, 4].map((i) => (
-            <_.MainSpotWrapper>
+            <_.BadmintonSpotWrapper>
               {[1, 2, 3, 4].map((i) => (
-                <_.MainSpotBtn bgColor="#80CBE3">
+                <_.BadmintonSpotBtn bgColor="#80CBE3">
                   <_.Text size={15} weight={700} color={"white"}>
                     지관
                   </_.Text>
-                </_.MainSpotBtn>
+                </_.BadmintonSpotBtn>
               ))}
-              <svg
-                width="4"
-                height="157"
-                viewBox="0 0 4 157"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-                style={{ position: "absolute", left: "50%" }}
-              >
-                <line
-                  x1="2"
-                  y1="156.338"
-                  x2="2"
-                  y2="0.645233"
-                  stroke="black"
-                  strokeWidth="3"
-                />
-              </svg>
-            </_.MainSpotWrapper>
+              <BadmintonLine />
+            </_.BadmintonSpotWrapper>
           ))}
-        </_.MainSpotContainer>
-      </_.MainContainer>
+        </_.BadmintonSpotContainer>
+      </_.BadmintonContainer>
     </>
   );
 }
-
-export function SideBar() {
+function BadmintonLine() {
+  return (
+    <svg
+      width="4"
+      height="157"
+      viewBox="0 0 4 157"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      style={{ position: "absolute", left: "50%" }}
+    >
+      <line
+        x1="2"
+        y1="156.338"
+        x2="2"
+        y2="0.645233"
+        stroke="black"
+        strokeWidth="3"
+      />
+    </svg>
+  );
+}
+function SideBar({ pathname }: { pathname: string }) {
   return (
     <_.SideContainer>
       <_.Text size={36} weight={700} height={44}>
         클럽
       </_.Text>
-      {[1, 2].map((i) => (
-        <_.SideBtnWrapper key={i}>
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "space-between",
-              alignContent: "center",
-            }}
-          >
-            <_.Text size={25} weight={700} height={43.57}>
-              클럽
-            </_.Text>
-            <MoonIcon />
-          </div>
-          <div
-            style={{
-              display: "flex",
-              boxShadow: "2px 2px 10px rgba(0, 0, 0, 0.15)",
-              borderRadius: "60px",
-            }}
-          >
-            <_.SideBtnColorStick width={100} />
-            <_.SideBtnGrayStick width={100} />
-          </div>
-          <div
-            style={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "space-between",
-            }}
-          >
-            <_.Text size={14} weight={600} color={"#898A8D"}>
-              00 : 00 : 00
-            </_.Text>
-            <_.Text size={18} weight={600} color={"#898A8D"}>
-              4/5명
-            </_.Text>
-          </div>
-        </_.SideBtnWrapper>
-      ))}
+      <SideBtn content="농구" link="basketball" pathname={pathname} />
+      <SideBtn content="배드민턴" link="badminton" pathname={pathname} />
+      <SideBtn content="축구" link="soccer" pathname={pathname} />
     </_.SideContainer>
+  );
+}
+
+function SideBtn({
+  content,
+  link,
+  pathname,
+}: {
+  content: string;
+  link: string;
+  pathname: string;
+}) {
+  return (
+    <Link to={`/club/${link}`}>
+      <_.SideBtnWrapper isUserClick={pathname.slice(6) === link}>
+        <div>
+          <_.Text size={25} weight={700} height={43.57}>
+            {content}
+          </_.Text>
+          <MoonIcon />
+        </div>
+        <div
+          style={{
+            display: "flex",
+            boxShadow: "2px 2px 10px rgba(0, 0, 0, 0.15)",
+            borderRadius: "60px",
+          }}
+        >
+          <_.SideBtnColorStick width={100} />
+          <_.SideBtnGrayStick width={100} />
+        </div>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+          }}
+        >
+          <_.Text size={14} weight={600} color={"#898A8D"}>
+            00 : 00 : 00
+          </_.Text>
+          <_.Text size={18} weight={600} color={"#898A8D"}>
+            4/5명
+          </_.Text>
+        </div>
+      </_.SideBtnWrapper>
+    </Link>
   );
 }
