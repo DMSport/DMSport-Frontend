382c18bebee0dbbad0e419cbda05cf2ca936e50f
jisoung <jangjisoung0405@gmail.com>
2022-11-09 19:03:54 +0900

    Feat : clubPage 라우터 추가

--- src/Router.tsx
+++ src/Router.tsx
@@ -6,6 +6,7 @@ import Privacy from "./Components/Privacy/Privacy";
 import NoticePage from "./Pages/NoticePage/NoticePage";
 import Main from "./Pages/main";
 import MyPage from "./Pages/MyPage";
+import ClubPage from "./Components/Club/ClubPage";
 
 function Router() {
   return (
@@ -16,8 +17,26 @@ function Router() {
         <Route path="/Privacy" element={<Privacy />} />
         <Route path="/PrivacyPolicy" element={<PrivacyPolicy />} />
         {/*404page*/}
-        <Route path="/notice" element={<NoticePage />} />
-        <Route path="/mypage" element={<MyPage />} />
+        <Route path="/notice" element={<NoticePage />}></Route>
+        <Route path="/mypage" element={<MyPage />}></Route>
+        <Route path="/club">
+          <Route
+            path="/club/soccer"
+            element={<ClubPage clubName={"soccer"} />}
+          />
+          <Route
+            path="basketball"
+            element={<ClubPage clubName={"basketball"} />}
+          />
+          <Route
+            path="badminton"
+            element={<ClubPage clubName={"badminton"} />}
+          />
+          <Route
+            path="volleyball"
+            element={<ClubPage clubName={"volleyball"} />}
+          />
+        </Route>
         <Route path="*" element="" />
       </Routes>
       <Footer />
